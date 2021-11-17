b40c471dae1d622322ba453769ba47ff0e87efe3
Đào Vũ Đạt <dat.daovu@gmail.com>
2021-11-17 19:37:41 +0700

    layout, header and footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {useRouter} from "next/router";
+import Layout from "../../components/Layout";
 
 const EventPage = () => {
+  const router = useRouter();
+  console.log(router);
+
+
   return (
-    <div>
+    <Layout>
       <h1>My event</h1>
-    </div>
+      <h3>{router.query['slug']}</h3>
+    </Layout>
   );
 };
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from "../../components/Layout";
 
 const EventsPage = () => {
   return (
-    <div>
+    <Layout>
       <h1>My events</h1>
-    </div>
+    </Layout>
   );
 };
 
